501f0c2dbfb7434812ef852b503ae4e3726eb613
cip <ciprian.trusca@gmail.com>
2014-05-01 21:50:07 +0300

    Initialise repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Incubator107::Application.config.secret_key_base = '13cc1e41ec5ae2e8fe3685fba607974fa6b0f11fd4ebad13827a05ee28ed783b612d8c2d713a63389fab755ff76fb1489786169e25fe6d35629f18c5449e0cec'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Incubator107::Application.config.session_store :cookie_store, key: '_incubator107_session'
