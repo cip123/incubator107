effdc23d82d1e5b215dd09298a87d59a6781ff8c
cip <ciprian.trusca@gmail.com>
2014-11-07 22:32:21 +0200

    removed turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require jquery.turbolinks
-//= require turbolinks
 //= require underscore
 //= require json2
 //= require judge
